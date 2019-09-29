9bd0d484f832944a25fd67caeb529da004b7471b
TzuTing112200 <51486061+TzuTing112200@users.noreply.github.com>
2019-09-30 02:32:21 +0800

    Update Enigma.cpp

--- Enigma.cpp
+++ Enigma.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <string>
 #include <time.h>
-#define N 12
+#define N 4
+#define SIZE 10395
 using namespace std;
+
+int cc = 0;
+int a[N];
+int group[SIZE][12];
 
 struct Rotor
 {
@@ -25,12 +30,6 @@ int PB_testCase[12];
 int change1[6] = { -1, -1,-1 ,-1 ,-1 ,-1 }, change2[6] = { -1,-1 ,-1 ,-1 ,-1 ,-1 };
 
 int input, output;
-
-int a[N]; 
-
-void perm(int); /*求陣列的全排列 */
-void printnum();
-void swap(int, int);
 
 //選盤子
 void set(int plate[], int starting[])
@@ -66,18 +65,8 @@ void set(int plate[], int starting[])
 	}
 }
 
-//加密
-void encrypt()
+void shift()
 {
-	/*------------------------------------對應插座------------------------------------------*/
-	for (int i = 0; i < 6; i++)
-	{
-		if (input == change1[i])
-			input = change2[i];
-		else if (input == change2[i])
-			input = change1[i];
-	}
-
 	/*------------------------------------設定加密方法------------------------------------------*/
 	//如果第二個指針碰到藍藍，第二、三個藍藍轉一個
 	if (rotor[1].current == rotor[1].pin)
@@ -92,6 +81,21 @@ void encrypt()
 
 	//第一個藍藍轉一個
 	rotor[0].current = (rotor[0].current + 1) % 26;
+}
+
+//加密
+void encrypt()
+{
+	/*------------------------------------對應插座------------------------------------------*/
+	for (int i = 0; i < 6; i++)
+	{
+		if (input == change1[i])
+			input = change2[i];
+		else if (input == change2[i])
+			input = change1[i];
+	}
+
+	shift();
 
 	/*------------------------------------找到加密文字------------------------------------------*/
 	//第一外圈的字母本人
@@ -166,62 +170,48 @@ void encrypt()
 	}
 }
 
-void perm(int offset) {
-	int i, temp;
-	if (offset == N / 2) {  // BaseCase
-		printnum();
-		return;
-	}
-	else {
-		for (i = offset; i < N; i++) {
-			swap(i, offset);//交換字首
-			perm(offset + 1);//遞迴
-			swap(i, offset);//將字首換回來，繼續做前一次排列
-		}
-	}
-}
-
-void printnum() 
+//測試
+void test()
 {
-	for (int i = 0; i < N; i += 2)
+	for (int index = 0; index < 10395; index++)
 	{
-		change1[i / 2] = a[i];
-		change2[i / 2] = a[i + 1];
-	}
-
-	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!大家改這裡喔!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	int plate[3] = { 1, X, X };
-	int starting[3];
-	for (int i = 0; i < 26; i++)
-	{
-		starting[0] = i;
-		for (int j = 0; j < 26; j++)
+		for (int ch = 0; ch < 12; ch += 2)
 		{
-			starting[1] = j;
-			for (int k = 0; k < 26; k++)
+			change1[ch / 2] = PB_testCase[group[index][ch]];
+			change2[ch / 2] = PB_testCase[group[index][ch + 1]];
+		}
+
+		//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!大家改這裡喔!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		int plate[3] = { 1, 3, 4 };
+		int starting[3];
+		for (int i = 0; i < 26; i++)
+		{
+			starting[0] = i;
+			for (int j = 0; j < 26; j++)
 			{
-				starting[2] = k;
-				set(plate, starting);
-
-				string user_input = "ZATVAWORBRQGRJSXZVNORWZBLORMEGRASLQLAFWXZYODVVTDHCIRDMNWOPNIXVKASIIIALOOSZXAMSYCQHYGPRLMSACGAWPCPAVZTMUUZCTJDVBUZAGFWMIVEZGBTLFIQDPPRZHDNKIPQHUGCXZM";
-				string user_output = "";
-
-				for (int i = 0; i < user_input.size(); i++)
+				starting[1] = j;
+				for (int k = 0; k < 26; k++)
 				{
-					input = int(user_input[i]) - 'A';
-					encrypt();
-					output += 'A';
-					if (i >= 138)
+					starting[2] = k;
+					set(plate, starting);
+
+					string user_input = "IPQHUGCXZM";
+					string answer = "HEILHITLER";
+					string user_output = "";
+
+					for (int m = 0; m < 138; m++)
+						shift();
+
+					for (int n = 0; n < 10; n++)
 					{
-						if (i == 138 && output != 'H')
+						input = int(user_input[n]) - 'A';
+						encrypt();
+						output += 'A';
+						if (output != answer[n])
 							break;
 						user_output += (char)output;
 					}
-				}
-				if (user_output != "") 
-				{
-					cout << user_output << endl;
-					if (user_output == "HEILHITLER")
+					if (user_output == answer)
 					{
 						cout << "--------------------------------------------------------\n";
 						cout << "Rotor：" << plate[0] << "\t" << plate[1] << "\t" << plate[2] << endl;
@@ -242,22 +232,114 @@ void printnum()
 }
 
 void swap(int i, int offset) {
+
 	int temp;
+
 	temp = a[offset];
+
 	a[offset] = a[i];
+
 	a[i] = temp;
+
 }
 
-//測試
-void test()
+void printnum(int pre[]) {
+	int i;
+	for (i = 0; i < 8; i++)
+		group[cc][i] = pre[i];
+
+	for (i = 0; i < N; i++)
+		group[cc][i + 8] = a[i];
+	cc++;
+}
+
+void perm(int pre[], int offset)
 {
-	//這邊要把PB_testCase選出來的12個倆倆組合後，丟到下面做
-	for (int i = 0; i < N; i++)
-		a[i] = PB_testCase[i];
-	perm(0);
+	int i;
+
+	if (offset == N / 2)
+	{  // BaseCase
+		printnum(pre);
+		return;
+	}
+	else
+	{
+		for (i = offset; i < N; i++)
+		{
+			swap(i, offset);//交換字首
+			perm(pre, offset + 1);//遞迴
+			swap(i, offset);//將字首換回來，繼續做前一次排列
+		}
+	}
 }
 
-void Combi(int a[], int reqLen, int s, int currLen, bool check[], int l)
+void go(int pre[], bool* b)
+{
+	int index = 0;
+	for (int i = 0; i < 12; i++)
+		if (b[i])
+			a[index++] = i;
+	perm(pre, 1);
+}
+
+void get_group()
+{
+	for (int i = 10; i >= 0; i--)
+	{
+		for (int j = 10; j >= 0; j--)
+		{
+			if (i != j)
+			{
+				for (int k = 10; k >= 0; k--)
+				{
+					if (k != i && k != j)
+					{
+						for (int l = 10; l >= 0; l--)
+						{
+							if (l != i && l != j && l != k)
+							{
+								for (int m = 10; m >= 0; m--)
+								{
+									if (m != i && m != j && m != k && m != l)
+									{
+										for (int n = 10; n >= 0; n--)
+										{
+											if (n != i && n != j && n != k && n != l && n != m)
+											{
+												for (int o = 10; o >= 0; o--)
+												{
+													if (o != i && o != j && o != k && o != l && o != m && o != n)
+													{
+														bool b[12] = { true,true,true,true,true,true,true,true,true,true,true,false };
+														b[i] = false;
+														b[j] = false;
+														b[k] = false;
+														b[l] = false;
+														b[m] = false;
+														b[n] = false;
+														b[o] = false;
+
+														int pre[8] = { 11,i,j,k,l,m,n,o };
+														go(pre, b);
+													}
+												}
+												break;
+											}
+										}
+									}
+								}
+								break;
+							}
+						}
+					}
+				}
+				break;
+			}
+		}
+	}
+}
+
+void Combi(int text[], int reqLen, int s, int currLen, bool check[], int l)
 {
 	if (currLen > reqLen)
 		return;
@@ -265,7 +347,7 @@ void Combi(int a[], int reqLen, int s, int currLen, bool check[], int l)
 		int index = 0;
 		for (int i = 0; i < l; i++) {
 			if (check[i] == true) {
-				PB_testCase[index++] = a[i];
+				PB_testCase[index++] = text[i];
 			}
 		}
 		test();
@@ -275,21 +357,22 @@ void Combi(int a[], int reqLen, int s, int currLen, bool check[], int l)
 		return;
 	}
 	check[s] = true;
-	Combi(a, reqLen, s + 1, currLen + 1, check, l);
+	Combi(text, reqLen, s + 1, currLen + 1, check, l);
 	check[s] = false;
-	Combi(a, reqLen, s + 1, currLen, check, l);
+	Combi(text, reqLen, s + 1, currLen, check, l);
 }
 
+
 int main() {
-	
-	int a[26];
+	get_group();
+	int text[26];
 	bool check[26];
 	for (int i = 0; i < 26; ++i)
 	{
-		a[i] = i;
+		text[i] = i;
 		check[i] = false;
 	}
-	Combi(a, 12, 0, 0, check, 26);
+	Combi(text, 12, 0, 0, check, 26);
 
 	return 0;
 }
